da3f87ea640339ccc2888016cf3cf1d3fc8be3cf
Zoddo <zoddo.ino@gmail.com>
2015-02-16 16:09:14 +0100

    Fix notices

--- DependencyInjection/OvhExtension.php
+++ DependencyInjection/OvhExtension.php
@@ -20,10 +20,10 @@ class OvhExtension extends Extension
 		$configuration = new Configuration();
 		$config = $this->processConfiguration($configuration, $configs);
 
-		$container->setParameter('ovh.key', $config['ovh_key']);
-		$container->setParameter('ovh.secret', $config['ovh_secret']);
-		$container->setParameter('ovh.endpoint', $config['ovh_endpointe']);
-		$container->setParameter('ovh.ckey', $config['ovh_ckey']);
+		$container->setParameter('ovh.key', (array_key_exists('ovh_key', $config )) ? $config['ovh_key'] : '');
+		$container->setParameter('ovh.secret', (array_key_exists('ovh_secret', $config )) ? $config['ovh_secret'] : '');
+		$container->setParameter('ovh.endpoint', (array_key_exists('ovh_endpointe', $config )) ? $config['ovh_endpointe'] : '');
+		$container->setParameter('ovh.ckey', (array_key_exists('ovh_ckey', $config )) ? $config['ovh_ckey'] : '');
 
 		$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 		$loader->load('services.yml');
